ca2c4de22456ad365512e70933802bb8b9ba39d2
Ben Marcotte <ben.marcotte99@gmail.com>
2022-08-03 03:24:31 -0400

    terrain generation v1

--- Final/main.cpp
+++ Final/main.cpp
@@ -17,6 +17,7 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow* window);
 void renderScene(Shader shader, Model obj);
+void generateTerrain();
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
@@ -31,7 +32,10 @@ bool firstMouse = true;
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
-// models
+// terrain data
+const int terrainHeight = 10;
+const int terrainWidth = 10;
+std::string terrainData[terrainHeight][terrainWidth];
 
 int main()
 {
@@ -71,6 +75,9 @@ int main()
         return -1;
     }
 
+    // generate terrain
+    generateTerrain();
+
     // tell stb_image.h to flip loaded texture's on the y-axis (before loading model).
     //stbi_set_flip_vertically_on_load(true);
 
@@ -87,6 +94,7 @@ int main()
     //Model ourModel("resources/objects/backpack/backpack.obj");
     //Model ourModel2("resources/objects/tree2/tree.obj");
     //Model ourModel3("resources/objects/tree3/tree4.obj");
+    Model ourModel4("resources/objects/shrine/shrine.obj");
 
 
     // draw in wireframe
@@ -121,7 +129,7 @@ int main()
         ourShader.setMat4("view", view);
 
         // render the loaded model
-        renderScene(ourShader, ourModel3);
+        renderScene(ourShader, ourModel4);
 
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
@@ -199,4 +207,128 @@ void renderScene(Shader shader, Model obj)
     model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));	// it's a bit too big for our scene, so scale it down
     shader.setMat4("model", model);
     obj.Draw(shader);
+}
+
+void generateTerrain()
+{
+    // pV == vertical path
+    // pH == horizontal path
+    // pNE == north-east path
+    // pSE == south-east path
+    // pSW == south-west path
+    // pNW == north-west path
+    // 1 == shrine
+    // 2 == bridge
+    // 3 == item3
+    // 4 == item4
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            terrainData[i][j] = "0";
+        }
+    }
+    
+
+    bool hasShrine = false;
+    bool hasBridge = false;
+    bool hasItem3 = false;
+    bool hasItem4 = false;
+    int pathRand;
+    int pathStart = rand() % 10;
+    int pathPos = pathStart;
+    terrainData[0][pathStart] = "pV";
+    bool columnHasPath = true;
+    srand(time(NULL));
+
+    for (int i = 0; i < 10; i++)
+    {
+        if (pathPos != 0 && pathPos != 9)
+        {
+            pathRand = (rand() % 3) - 1;
+        }
+        else if (pathPos == 0)
+        {
+            pathRand = (rand() % 2);
+        }
+        else
+        {
+            pathRand = (rand() % 2) - 1;
+        }
+        for (int j = 0; j < 10; j++) 
+        {
+            if (columnHasPath != true)
+            {
+                if (pathRand + pathPos == j)
+                {
+                    if (pathRand == -1)
+                    {
+                        terrainData[i][j] = "pSW";
+                        columnHasPath = true;
+                        pathPos = j + pathRand;
+                    }
+                    else if (pathRand == 0)
+                    {
+                        terrainData[i][j] = "pV";
+                        columnHasPath = true;
+                        pathPos = j + pathRand;
+                    }
+                    else
+                    {
+                        terrainData[i][j] = "pSE";
+                        columnHasPath = true;
+                        pathPos = j + pathRand;
+                    }
+                }
+            }
+            
+            if (terrainData[i][j].compare("0") == 0 && (hasShrine == false || hasBridge == false || hasItem3 == false || hasItem4 == false))
+            {
+                //int g = rand() % 25;
+                //cout << g << endl;
+                if (rand() % 25 == 0)
+                {
+                    bool itemTaken = false;
+                    while (!itemTaken)
+                    {
+                        int t = (rand() % 4) + 1;
+                        if (t == 1 && hasShrine == false)
+                        {
+                            terrainData[i][j] = "1";
+                            hasShrine = true;
+                            itemTaken = true;
+                        }
+                        if (t == 2 && hasBridge == false)
+                        {
+                            terrainData[i][j] = "2";
+                            hasBridge = true;
+                            itemTaken = true;
+                        }
+                        if (t == 3 && hasItem3 == false)
+                        {
+                            terrainData[i][j] = "3";
+                            hasItem3 = true;
+                            itemTaken = true;
+                        }
+                        if (t == 4 && hasItem4 == false)
+                        {
+                            terrainData[i][j] = "4";
+                            hasItem4 = true;
+                            itemTaken = true;
+                        }
+                    }
+                }
+            }
+        }
+        columnHasPath = false;
+    }
+    for (int i = 0; i < terrainHeight; ++i)
+    {
+        for (int j = 0; j < terrainWidth; ++j)
+        {
+            std::cout << terrainData[i][j] << ' ';
+        }
+        std::cout << std::endl;
+    }
+
 }
